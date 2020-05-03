f34166e5853590880fed08b1d3518583be475baa
Geethanjali Thenisseri Veetil <gtveetil@gmail.com>
2020-05-03 11:39:02 +1000

    Updated field sizes

--- db/schema.sql
+++ db/schema.sql
@@ -4,13 +4,13 @@ USE employee_db;
 
 CREATE TABLE department(
   id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(30) NOT NULL,
+  name VARCHAR(100) NOT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE `role`(
   id INT NOT NULL AUTO_INCREMENT,
-  title VARCHAR(30) NOT NULL,
+  title VARCHAR(100) NOT NULL,
   salary DECIMAL,
   department_id INT NOT NULL,
   PRIMARY KEY (id),
@@ -19,8 +19,8 @@ CREATE TABLE `role`(
 
 CREATE TABLE employee(
   id INT NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
+  first_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100) NOT NULL,
   role_id INT NOT NULL,
   manager_id INT NULL,
   PRIMARY KEY (id),
